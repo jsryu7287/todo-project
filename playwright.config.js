6c24c7aa74f04ecc49e4a38948b1d4b6715e352e
jsryu7287 <jsryu7287@gmail.com>
2026-06-04 14:22:06 +0900

    chore: remove playwright.config.js

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-
-module.exports = defineConfig({
-  testMatch: '**/*.test.js',
-  use: {
-    headless: true,
-  },
-  reporter: [['list']],
-});
